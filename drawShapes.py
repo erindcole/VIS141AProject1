c587bc875a408478091aa359907d194fe389c0ce
Erin Cole <Erin Cole>
2017-06-04 16:47:12 -0700

    Added some comments and function headers

--- drawShapes.py
+++ drawShapes.py
@@ -1,7 +1,14 @@
+# Erin Cole
+# This program contains the functions that draw different shapes, when called.
+
 from PIL import Image
 from Lib import random
 import time
 
+
+# Function name: randomize
+# This function will randomly make either a circle or a square, in accordance
+# with a random seed
 def randomize():
     randVal = random.random()
 
@@ -11,11 +18,15 @@ def randomize():
     else:
         makeSquare()
 
+# Function name: makesquare
+# This function will show a square.jpg file
 def makeSquare():
     square = Image.open('square.jpg')
     square.show()
 
 
+# Function name: makeCircle
+# This function will show a circle.jpg file
 def makeCircle():
     circle = Image.open('circle.jpg')
     circle.show()
